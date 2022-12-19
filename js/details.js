2159c9eb0835d3ebf137592fcd1cbce31a9b9b0e
Roselene Gabun <roselenegabun@gmail.com>
2022-12-19 11:56:16 -0800

    Refractor Code

--- js/details.js
+++ js/details.js
@@ -1,5 +1,17 @@
 "use strict"
 
+// Init Vars Here
+const productNameField = document.getElementById("productNameField");
+const productIDField = document.getElementById("prodID");
+const productCostField = document.getElementById("prodCost");
+const productStockField = document.getElementById("prodStock");
+const productSupplyField = document.getElementById("prodSupply");
+const productDiscField = document.getElementById("prodDisc");
+const categoryImg = document.getElementById("categoryImgField");
+const categoryArray = ["drinks", "condiments", "confections", "dairy",
+                    "grains", "meats", "produce", "seafood"]
+
+// OnLoad, init wow animations + fetch data from API
 window.addEventListener('load', function(){
     new WOW().init();
     const urlParam = new URLSearchParams(location.search)
@@ -17,29 +29,18 @@ window.addEventListener('load', function(){
     }
 })
 
+// Once Everything is Loaded, Display Data on Page
 function showProductInfo(data) {
-    const productNameField = document.getElementById("productNameField");
-    const productIDField = document.getElementById("prodID");
-    const productCostField = document.getElementById("prodCost");
-    const productStockField = document.getElementById("prodStock");
-    const productSupplyField = document.getElementById("prodSupply");
-    const productDiscField = document.getElementById("prodDisc");
-    const categoryImg = document.getElementById("categoryImgField");
-    // Array for Category's Images
-    const categoryArray = ["drinks", "condiments", "confections", "dairy",
-                        "grains", "meats", "produce", "seafood"]
-
     productNameField.innerHTML = data.productName;
     productIDField.innerHTML = data.productId;
     productCostField.innerHTML = `$${(parseFloat(data.unitPrice)).toFixed(2)}`;
     productStockField.innerHTML = data.unitsInStock;
     productSupplyField.innerHTML = data.supplier;
+    categoryImg.src =`../images/categories/${categoryArray[data["categoryId"] - 1]}.jpg`
 
     if (data.discontinued == 'true') {
-        productDiscField.innerHTML = "Sorry, we no longer have this product."
+        productDiscField.innerHTML = "Sorry, this product has been discontinued."
     } else {
         productDiscField.innerHTML = "In Stock"
     }
-
-    categoryImg.src =`../images/categories/${categoryArray[data["categoryId"] - 1]}.jpg`
 }
